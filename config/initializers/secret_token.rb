188cc80a8f85e83ac990e5991f6ec569fef9990f
Mark Leetch <markleetch@gmail.com>
2013-06-23 02:01:18 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquadXML::Application.config.secret_token = 'e0b1103339818513ac252d5c10b51733e8fd90ffe6fe44a5e222d7f3f40cc8100977627012d932f12dc65704504885af8748be4adeab003a238199c1205c1e0a'
