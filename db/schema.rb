44e575e6ce95d7ae0dcbb43ce9d1e4d9179bc8b0
Mark Leetch <markleetch@gmail.com>
2013-06-23 03:53:35 +0100

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623021426) do
+ActiveRecord::Schema.define(:version => 20130623025257) do
+
+  create_table "squad_members", :force => true do |t|
+    t.integer  "uid"
+    t.string   "nickname"
+    t.string   "name"
+    t.string   "email"
+    t.integer  "icq"
+    t.string   "remark"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "squads", :force => true do |t|
+    t.string   "name"
+    t.string   "tag"
+    t.string   "email"
+    t.string   "website"
+    t.string   "picture"
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
