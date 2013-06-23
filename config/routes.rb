d819a6b2475fb40f21da72357ba6fb075a2ceb7f
Mark Leetch <markleetch@gmail.com>
2013-06-23 03:24:52 +0100

    Installing simple_form correctly and generating the devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SquadXML::Application.routes.draw do
   devise_for :users
-
+  devise_scope :user do
+    match "/login" => "devise/sessions#new"
+    match "/logout" => "devise/sessions#destroy"
+    match "/signup" => "devise/registrations#new"
+  end
 end
